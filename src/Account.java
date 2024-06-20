c5f679326b52a04dee4ca8325a626211085ebf65
Dev_Breezy <nwabuikecharles@gmail.com>
2024-06-20 02:58:17 -0400

    first commit

---
+++ src/Account.java
@@ -0,0 +1,62 @@
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private final String customerName;
+    private int accountNumber;
+    private double checkingBalance = 0;
+    private double savingsBalance = 0;
+    private int accountPin;
+
+    public Account(int accountNumber, String customerName) {
+        this.accountNumber = accountNumber;
+        this.customerName = customerName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountPin() {
+        return accountPin;
+    }
+
+    public void setAccountPin(int accountPin) {
+        this.accountPin = accountPin;
+    }
+
+    public void withdrawSavings(double amount) {
+        if (savingsBalance - amount < 0) {
+            System.out.println("Insufficient funds");
+        } else {
+            savingsBalance -= amount;
+            System.out.println("Withdrawal of " + amount + " processed. Remaining savings balance: " + savingsBalance);
+        }
+    }
+
+    public void depositSavings(double amount) {
+        savingsBalance += amount;
+        System.out.println("Deposit of " + amount + " processed. New savings balance: " + savingsBalance);
+    }
+
+    public void withdrawChecking(double amount) {
+        if (checkingBalance - amount < 0) {
+            System.out.println("Insufficient funds");
+        } else {
+            checkingBalance -= amount;
+            System.out.println("Withdrawal of " + amount + " processed. Remaining checking balance: " + checkingBalance);
+        }
+    }
+
+    public void depositChecking(double amount) {
+        checkingBalance += amount;
+        System.out.println("Deposit of " + amount + " processed. New checking balance: " + checkingBalance);
+    }
+}
